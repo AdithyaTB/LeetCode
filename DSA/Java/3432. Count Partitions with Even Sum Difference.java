d371bb0e2c01cdc83ed7afca22708d59c39097d7
Adithya T B <168957650+AdithyaTB@users.noreply.github.com>
2025-12-05 22:04:02 +0530

    Create 3432. Count Partitions with Even Sum Difference.java

---
+++ DSA/Java/3432. Count Partitions with Even Sum Difference.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countPartitions(int[] nums) {
+        int total = 0;
+        for(int n : nums) {
+            total += n;
+        }
+        int leftSum = 0 , count = 0;
+        for (int i =0; i < nums.length - 1; i++) {
+            leftSum += nums[i];
+            int rightSum = total - leftSum;
+            if ((leftSum % 2) == (rightSum % 2)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
